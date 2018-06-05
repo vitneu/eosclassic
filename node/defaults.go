75324397c42629d711d77eab8467ed8202f11b79
EOS Classic <info@eos-classic.io>
2018-06-06 04:50:18 +0900

    [DIR] Change default datadir

--- node/defaults.go
+++ node/defaults.go
@@ -55,11 +55,11 @@ func DefaultDataDir() string {
 	home := homeDir()
 	if home != "" {
 		if runtime.GOOS == "darwin" {
-			return filepath.Join(home, "Library", "Ethereum")
+			return filepath.Join(home, "Library", "EOSClassic")
 		} else if runtime.GOOS == "windows" {
-			return filepath.Join(home, "AppData", "Roaming", "Ethereum")
+			return filepath.Join(home, "AppData", "Roaming", "EOSClassic")
 		} else {
-			return filepath.Join(home, ".ethereum")
+			return filepath.Join(home, ".eosclassic")
 		}
 	}
 	// As we cannot guess a stable location, return empty and handle later

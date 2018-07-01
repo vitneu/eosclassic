eabc42a6825d728864a10bef683e5f2ca61c42bc
EOS Classic <info@eos-classic.io>
2018-07-01 17:08:36 +0900

    VERSION, params: begin v1.8.15 release cycle

--- params/version.go
+++ params/version.go
@@ -23,8 +23,8 @@ import (
 const (
 	VersionMajor = 1      // Major version component of the current release
 	VersionMinor = 8      // Minor version component of the current release
-	VersionPatch = 14     // Patch version component of the current release
-	VersionMeta  = "eosc" // Version metadata to append to the version string
+	VersionPatch = 15     // Patch version component of the current release
+	VersionMeta  = "test" // Version metadata to append to the version string
 )
 
 // Version holds the textual version string.

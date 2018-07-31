ebb29c6acfbf0464f809d14e8eebaf648a899149
EOS Classic <info@eos-classic.io>
2018-07-31 23:21:47 +0900

    [Rebrand] Fix console message

--- cmd/geth/consolecmd_test.go
+++ cmd/geth/consolecmd_test.go
@@ -56,7 +56,7 @@ func TestConsoleWelcome(t *testing.T) {
 
 	// Verify the actual welcome message to the required template
 	geth.Expect(`
-Welcome to the Geth JavaScript console!
+Welcome to the EOSC JavaScript console!
 
 instance: Geth/v{{gethver}}/{{goos}}-{{goarch}}/{{gover}}
 coinbase: {{.Etherbase}}

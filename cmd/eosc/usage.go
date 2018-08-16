bc79ff50eb01f4b62f2fccd057f74e4d4db0b4e4
EOS Classic <info@eos-classic.io>
2018-08-16 19:12:12 +0900

    ETHEREUM -> EOS CLASSIC

--- cmd/eosc/usage.go
+++ cmd/eosc/usage.go
@@ -65,7 +65,7 @@ type flagGroup struct {
 // AppHelpFlagGroups is the application flags, grouped by functionality.
 var AppHelpFlagGroups = []flagGroup{
 	{
-		Name: "ETHEREUM",
+		Name: "EOS CLASSIC",
 		Flags: []cli.Flag{
 			configFileFlag,
 			utils.DataDirFlag,
